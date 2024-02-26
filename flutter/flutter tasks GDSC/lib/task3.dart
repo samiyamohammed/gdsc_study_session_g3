f7ec38f33144d587bc3405d21c1d8cefc45efb41
samiyamohammed <121340936+samiyamohammed@users.noreply.github.com>
2024-02-26 16:16:52 +0300

    Update task3.dart

--- flutter/flutter tasks GDSC/lib/task3.dart
+++ flutter/flutter tasks GDSC/lib/task3.dart
@@ -27,26 +27,19 @@ void main() async {
   try {
     double result;
 
-    // Example: performing addition
     result = calculator.add(10, 5);
     print('Addition result: $result');
-
-    // Example: performing subtraction
     result = calculator.subtract(10, 5);
     print('Subtraction result: $result');
 
-    // Example: performing multiplication
     result = calculator.multiply(10, 5);
     print('Multiplication result: $result');
 
-    // Example: performing division
     result = calculator.divide(10, 2);
     print('Division result: $result');
 
-    // Example: handling division by zero
     result = calculator.divide(10, 0);
 
-    // The program should print the result after a 5-second delay
     await Future.delayed(const Duration(seconds: 5));
     print('Result after a 5-second delay: $result');
   } catch (e) {
